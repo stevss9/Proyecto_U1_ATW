82555d46ec18de15b22b925f3d67cec026541daf
Steveen <sinchiguanomoreira@gmail.com>
2022-11-30 18:35:36 -0500

    FINAL COMMIT

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python -tt
+# Importar Flask
 import time
 from flask import Flask, redirect, render_template, request
 # Instancia de Flask. Aplicación
@@ -9,7 +10,40 @@ app = Flask(__name__, template_folder='template')
 @app.route('/')
 # Función para llamar a la página index.html
 def index():
-    return render_template("skincare.html")
+    return render_template("principal.html")
+
+
+
+# Define la ruta principal Lucy Mosquera
+@app.route('/LM')
+# Función para llamar a la página index.html
+def LM():
+    return render_template("LM.html")
+
+
+# Define la ruta principal Mercy Arrobo
+@app.route('/MA')
+# Función para llamar a la página index.html
+def MA():
+    return render_template("MA.html")
+
+# Define la ruta principal Steveen Sinchiguano
+@app.route('/SS')
+# Función para llamar a la página index.html
+def SS():
+    return render_template("SS.html")
+
+# Define la ruta principal Mary Guarnizo
+@app.route('/MG')
+# Función para llamar a la página index.html
+def MG():
+    return render_template("MG.html")
+
+# Define la ruta principal Rachel Garces
+@app.route('/RG')
+# Función para llamar a la página index.html
+def RG():
+    return render_template("RG.html")
 
 
 # main del programa
